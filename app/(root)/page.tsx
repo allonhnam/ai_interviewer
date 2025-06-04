cca06b0b97ab286075f6543015c4c2b1eb425f95
Allon Nam <allon.nam96@gmail.com>
2025-06-03 20:59:27 -0400

    working on navbar

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,8 +1,29 @@
+import { Button } from '@/components/ui/button'
+import Image from 'next/image'
+import Link from 'next/link'
 import React from 'react'
 
 const page = () => {
   return (
-    <div>Home Page</div>
+    <>
+      <section className='card-cta'>
+        <div className='flex flex-col gap-6 max-2-lg'>
+          <h2>Get Ready with AI-Powered Mock Interview Practice & Feedback</h2>
+          <p className='text-lg'>
+            Practive on real interview questions & get instant feedback
+          </p>
+
+          <Button asChild className="btn-primary max-sm:w-full">
+            <Link href="/interview">Start an Interview</Link>
+          </Button>
+        </div>
+        <Image src="/robot.png" alt="robo-dude" width={400} height={400} className='max-sm:hidden' />
+      </section>
+      <section className='flex flex-col gap-6 mt-8'>
+        
+
+      </section>
+    </>
   )
 }
 
